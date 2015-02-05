e652f9cae3f786234a23addf49190101650a8062
Michael Tyson <michael@atastypixel.com>
2015-02-05 12:36:20 +1100

    Refactored background gradient out into SEGraphics

--- Syncy/SEBackgroundView.m
+++ Syncy/SEBackgroundView.m
@@ -7,30 +7,21 @@
 //
 
 #import "SEBackgroundView.h"
+#import "SEGraphics.h"
 
 @implementation SEBackgroundView
 
 -(void)drawRect:(CGRect)rect {
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    UIColor* color = [UIColor colorWithRed: 0.1 green: 0.064 blue: 0.001 alpha: 1];
-    UIColor* color2 = [UIColor colorWithRed: 0.29 green: 0.159 blue: 0.004 alpha: 1];
-    
-    CGFloat radialGradient1Locations[] = {0, 1};
-    CGGradientRef radialGradient1 = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)@[(id)color2.CGColor, (id)color.CGColor], radialGradient1Locations);
-    
+    CGGradientRef radialGradient = [SEGraphics backgroundGradient].CGGradient;
     UIBezierPath* backgroundPath = [UIBezierPath bezierPathWithRect: CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height)];
     CGContextSaveGState(context);
     [backgroundPath addClip];
-    CGContextDrawRadialGradient(context, radialGradient1,
+    CGContextDrawRadialGradient(context, radialGradient,
                                 CGPointMake(self.bounds.size.width/2.0, self.bounds.size.height/2.0), 0,
                                 CGPointMake(self.bounds.size.width/2.0, self.bounds.size.height/2.0), self.bounds.size.height,
                                 kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
     CGContextRestoreGState(context);
-    
-    CGGradientRelease(radialGradient1);
-    CGColorSpaceRelease(colorSpace);
 }
 
 @end
